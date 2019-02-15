52897c1071ec1b49d16d1e236fa63370ae189dda
RetroPie-Expanded <46853246+RetroPie-Expanded@users.noreply.github.com>
2019-02-15 09:05:50 -0800

    Create update-retropexpanded.sh

---
+++ scriptmodules/supplementary/update-retropexpanded.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+##############
+
+rp_module_id="update-retropexpanded"
+rp_module_desc="Easily update RetroPie-Expanded"
+rp_module_help="Follow the instructions on the dialogs"
+rp_module_section="exp"
+
+function sources_update-retropexpanded() {
+    wget https://raw.githubusercontent.com/RetroPie-Expanded/RetroPie-Expanded/master/update-retropexpanded.sh
+}
+
+function install_update-retropexpanded() {
+
+    cp ./update-retropexpanded.sh "$home/RetroPie/retropiemenu"
+    chown -R $user:$user "$datadir/retropiemenu"
+}
+
+function remove_update-retropexpanded() {
+    rm -rfv "$datadir/retropiemenu/update-retropexpanded.sh"
+}
+
+function gui_update-retropexpanded() {
+    bash "$md_inst/update-retropexpanded.sh"
+}
